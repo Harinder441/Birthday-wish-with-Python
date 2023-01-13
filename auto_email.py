a7d6d6b6e32a33fde9b20d8ef748c93235737120
Harinder Singh <99085182+Harinder441@users.noreply.github.com>
2023-01-13 19:52:27 +0530

    Update auto_email.py

--- auto_email.py
+++ auto_email.py
@@ -3,7 +3,7 @@
 import smtplib
 
 
-def send_mail(to_ad, massage,from_ad="harryautoemail@gmail.com", password="dgtkeudhnbowvhli"):
+def send_mail(to_ad, massage,from_ad=YOUR_EMAIL_ID, password= YOUR_APP_PASSWORD):
     with smtplib.SMTP("smtp.gmail.com") as con:
         # securing
         con.starttls()
@@ -12,13 +12,3 @@ def send_mail(to_ad, massage,from_ad="harryautoemail@gmail.com", password="dgtke
         con.sendmail(from_addr=from_ad, to_addrs=to_ad, msg=massage)
 
 
-if __name__ == "__main__":
-    my_email = "harryautoemail@gmail.com"
-    passw = "dgtkeudhnbowvhli"
-    msg = "Subject:Hello Sahil \n\n greeting from Harry auto mail"
-    to = "basant29082001@gmail.com"
-    i = 1
-    while (i < 100):
-        msg = f"Subject:Hello{i} Basant Singh \n\n greeting from Harry auto mail {i}"
-        # send_mail(massage=msg)
-        i += 1
