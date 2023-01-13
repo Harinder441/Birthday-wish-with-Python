8341ca32a07938730ad3794a2cf6d8b6914d2c10
Harinder Singh <99085182+Harinder441@users.noreply.github.com>
2023-01-13 19:54:30 +0530

    Update auto_email.py

--- auto_email.py
+++ auto_email.py
@@ -10,5 +10,15 @@ def send_mail(to_ad, massage,from_ad=YOUR_EMAIL_ID, password= YOUR_APP_PASSWORD)
         # login
         con.login(user=from_ad, password=password)
         con.sendmail(from_addr=from_ad, to_addrs=to_ad, msg=massage)
+        
+        
+def msg_format(from_p="", to="", body="",subject=""):
+    msg = f"""From:{from_p} 
+        Subject: {subject} 
+
+
+        {body} 
+        """
+    return msg
 
 
